0b2c98ccebe443546779454deaf5d2ba536484ef
MatanelP <75391764+MatanelP@users.noreply.github.com>
2021-06-16 17:38:08 +0300

    test

---
+++ vl_string.h
@@ -0,0 +1,8 @@
+//
+// Created by Matanel on 16/06/2021.
+//
+
+#ifndef _VL_STRING_H_
+#define _VL_STRING_H_
+
+#endif //_VL_STRING_H_
--- vl_vector.h
+++ vl_vector.h
@@ -5,4 +5,7 @@
 #ifndef _VL_VECTOR_H_
 #define _VL_VECTOR_H_
 
+
+
+
 #endif //_VL_VECTOR_H_
